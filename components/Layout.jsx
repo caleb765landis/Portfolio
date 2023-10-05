67acf1fd112f68887eb7f0c57b0a293c3b709edf
caleb765landis <caleb765landis@gmail.com>
2023-10-05 12:11:21 -0600

    Updated hero

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import Header from "./Header";
 import Footer from "./Footer";
 
@@ -9,7 +10,11 @@ export const metadata = {
 export default function Layout({children}) {
     return (
         <>
+        <Head>
+            <title>{"Caleb Landis - Personal Portfolio"}</title>
+        </Head>
         <div className="bg-primary">
+        {/* <div className="bg-gradient-to-br from-secondary from-30% to-blue-300"> */}
             <Header />
             <main>{children}</main>
             <Footer />
--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -1,24 +1,36 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Hero() {
     return (
-    <section id="hero" className="h-screen bg-secondary text-primary p-10 grid md:grid-cols-2">
-        <div className="flex items-end md:justify-center md:items-center">
+    <section id="hero" className="h-screen bg-gradient-to-br from-secondary from-50% to-slate-300 bg-blend-multiply text-primary p-10 grid md:grid-cols-2">
+        <div id="intro" className="flex items-start md:items-center md:justify-center order-last md:order-first">
             <div className="flex flex-col">
-                <p className="text-4xl">Howdy! My name is</p>
-                <p className="text-8xl font-bold pb-1">Caleb Landis.</p>
-                <p className="text-3xl pb-5 md:pb-0">Welcome to my portfolio.</p>
+                <div>
+                    <p className="text-4xl">Howdy! My name is</p>
+                    <p className="text-8xl font-bold pb-1">Caleb Landis.</p>
+                    <p className="text-3xl pb-5">Welcome to my portfolio.</p>
+                </div>
+                
+                <div className="pb-5">
+                    <button type="button" className="text-white bg-primary hover:bg-accent focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                        <Link href="/#about">Get to know me.</Link>
+                    </button>
+                    <button type="button" className="text-primary bg-white border-2 border-primary hover:bg-accent focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                        <Link href="/contact/">Contact me.</Link>
+                    </button>
+                </div>
             </div>
         </div>
-        <div className="md:flex md:justify-center md:items-center">
-            <div className="flex md:flex-col">
-                <button type="button" className="text-white bg-primary hover:bg-accent focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
-                    <Link href="/#about">Get to know me.</Link>
-                </button>
-                <button type="button" className="text-primary bg-white border-2 border-primary hover:bg-accent focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
-                    <Link href="/contact/">Contact me.</Link>
-                </button>
-            </div>
+        <div id="profilePic" className="flex justify-center items-center">
+            <Image
+                priority
+                src="/images/profile.jpg"
+                className="rounded-full border-8 border-primary"
+                height={288}
+                width={288}
+                alt="Profile Photo"
+            />
         </div>
     </section>
     );
