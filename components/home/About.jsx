82854b1f14b9f743c157d58d36f5d4e305839044
caleb765landis <caleb765landis@gmail.com>
2023-10-07 11:00:36 -0600

    Layed out grid for about section.

--- components/home/About.jsx
+++ components/home/About.jsx
@@ -1,31 +1,35 @@
+import Image from 'next/image'
+
 export default function About() {
     return (
         <>
-        <div id="about" className="h-screen p-20 items-center justify-items-center">
-            {/* <div className="grid columns-1"> */}
+            <div id="about" className="h-screen p-20 flex flex-col items-stretch justify-center">
+                <div className="flex justify-center text-5xl pb-10">About Me</div>
 
-            <h1 className="flex justify-center text-5xl pb-10">About Me</h1>
-            {/* <div className="flex-1"> */}
-
-            <div className="bg-green-500 grid md:grid-flow-col gap-5">
-                <div className="card row-span-2">
-                {/* <div className="card row-span-2 "> */}
-                    Photo
-                </div>
-                {/* <div> */}
+                <div className="grid md:grid-flow-col gap-5 m-3 grow">
+                    <div id="aboutPic" className="card row-span-2">
+                        <div id="profilePic" className="flex justify-center items-center">
+                            Profile
+                            {/* <Image
+                                priority
+                                src="/images/profile.jpg"
+                                className=""
+                                // height={document.getElementById("aboutPic").getBoundingClientRect().height}
+                                // width={document.getElementById("aboutPic").getBoundingClientRect().width}
+                                height={400}
+                                width={400}
+                                alt="Profile Photo"
+                            /> */}
+                        </div>
+                    </div>
                     <div className="card">
                         About
                     </div>
                     <div className="card">
                         Soft Skills
                     </div>
-                {/* </div> */}
+                </div>
             </div>
-            
-            {/* </div> */}
-            
-            {/* </div> */}
-        </div>
         </>
     );
 }
