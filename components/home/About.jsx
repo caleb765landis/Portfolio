5c8b7675173907c3873224c27f4d17b39a0582bd
caleb765landis <caleb765landis@gmail.com>
2023-10-26 17:16:16 -0600

    Started featured projects layout and cards

--- components/home/About.jsx
+++ components/home/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
             <div id="about" className="min-h-screen md:p-20 p-10 flex flex-wrap justify-center">
                 {/* Introduction Section */}
                 <div className="content-center flex flex-col justify-center">
-                    <div className="flex justify-center text-5xl md:p-0 md:pb-10 pb-10"><h1>About Me</h1></div>
+                    <div className="flex justify-center text-6xl font-semibold md:pb-10 pb-10"><h1>About Me</h1></div>
                     <div className="flex justify-center md:text-center text-xl pb-10">
                         <p>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -32,13 +32,13 @@ export default function About() {
 
                         {/* Skills Cards */}
                         <div className="card">
-                            <h1 className="text-4xl pb-4">Soft Skills</h1>
+                            <h1 className="text-4xl font-semibold pb-4">Soft Skills</h1>
                             <p>
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                             </p>
                         </div>
                         <div className="card">
-                            <h1 className="text-4xl pb-4">Hard Skills</h1>
+                            <h1 className="text-4xl font-semibold pb-4">Hard Skills</h1>
                             <p>
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                             </p>
